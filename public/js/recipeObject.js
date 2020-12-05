82a29532047cb7f77051b1f42952be6b0e718597
Dee Wu <DeeWu@MacBook-Pro-9.local>
2020-12-05 16:53:10 +0800

    form

--- public/js/recipeObject.js
+++ public/js/recipeObject.js
@@ -71,7 +71,10 @@ function recipeToString(recipe)
 
 	// favoriteHiddenDiv = "<input type='text' name='favorite' id='favoriteId' style='display:none' />";
 	html += nameString + html2 + imageString + html3 + instructionsString + html4 + ingredientsString;
-	html += "</div></div>";
+	html += "<form class="form1" method="POST" action="">";
+	html += "<input type="button" value="Add to Favorites" id="addToFavorites"><br/><br/>";
+	html += "<input type="button" value="NEXT RECIPE" id="refreshRecipesButton"/></form>";
+	html += "</div></div></div>";
 	html += "</div>";
 
 	return html;
