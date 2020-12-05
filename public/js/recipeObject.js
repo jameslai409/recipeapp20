a5dc928ece73864fbd6a5992296d0ec5362245d3
Dee Wu <DeeWu@MacBook-Pro-9.local>
2020-12-05 19:08:03 +0800

    d

--- public/js/recipeObject.js
+++ public/js/recipeObject.js
@@ -68,8 +68,7 @@ function recipeToString(recipe)
 	instructionsString = "<div class='instructions'>" + recipe.instructions + "</div>";
 	ingredientsString = "<div class='ingredients'>" + arrayToUnorderedList(recipe.ingredients) + "</div>";
 	// favoriteHiddenDiv = "<input type='text' name='favorite' id='favoriteId' style='display:none' />";
-	html_holder = "<div id='form_holder'></div>";
-	html += nameString + html2 + imageString + html3 + instructionsString + ingredientsString + html_holder;
+	html += nameString + html2 + imageString + html3 + instructionsString + ingredientsString;
 	html += "</div></div>";
 	return html;
 }
