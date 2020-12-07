236825b10952bf7ec808a295c25780c66830c078
edebar01 <erica.debarge@tufts.edu>
2020-12-06 20:06:15 -0500

    e

--- server.js
+++ server.js
@@ -66,27 +66,27 @@ app.post('/createrecipe', function(req, res) {
 app.post('/webrecipe', function(req, res) {
     console.log("in webrecipe endpoint");
     //parses the recipe passed through the POST request
-    var body = "";
-    req.on('error', function(err) {
-      // This prints the error message and stack trace to `stderr`.
-      console.error(err.stack);
-    });
-    req.on("data", function(data) {
-        console.log("in req.on 'data'");
-        body += data;
-    });
-    req.on("end", function() {
-        console.log("in req.on 'end'");
-        var recipe = querystring.parse(body);
-        var dishTypesArray = toArray(recipe.dishTypes);
-        var cuisineTypesArray = toArray(recipe.cuisineTypes);
-        var ingredientsArray = toArray(recipe.ingredients);
-        recipe.dishTypes = dishTypesArray;
-        recipe.cuisineTypes = cuisineTypesArray;
-        recipe.ingredients = ingredientsArray;
-        console.log("before insertRecipe");
-        insertRecipe(recipe);
-    });
+    // var body = "";
+    // req.on('error', function(err) {
+    //   // This prints the error message and stack trace to `stderr`.
+    //   console.error(err.stack);
+    // });
+    // req.on("data", function(data) {
+    //     console.log("in req.on 'data'");
+    //     body += data;
+    // });
+    // req.on("end", function() {
+    //     console.log("in req.on 'end'");
+    //     var recipe = querystring.parse(body);
+    //     var dishTypesArray = toArray(recipe.dishTypes);
+    //     var cuisineTypesArray = toArray(recipe.cuisineTypes);
+    //     var ingredientsArray = toArray(recipe.ingredients);
+    //     recipe.dishTypes = dishTypesArray;
+    //     recipe.cuisineTypes = cuisineTypesArray;
+    //     recipe.ingredients = ingredientsArray;
+    //     console.log("before insertRecipe");
+    //     insertRecipe(recipe);
+    // });
 
     //redirect to same page. gets rid of POST shenanigans (timeouts)
     res.redirect('webrecipe');
